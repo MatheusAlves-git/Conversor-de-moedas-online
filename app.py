2f69c6bc0e9cca6a6a633ab4b8612342da850912
Matheus Alves <matheusalves.aa@outlook.com>
2020-07-10 15:04:46 -0300

    Cruzando os dados da tabela com o comando set_index

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from arq import Aplicacao
 
 janela = tk.Tk()
+janela.title('Conversor de Moeda')
+janela.config(bg='red')
 Aplicacao(janela).grid()
 
 janela.resizable(0, 0)
--- arq.py
+++ arq.py
@@ -40,23 +40,20 @@ class Aplicacao(Frame):
             caixa1 = caixa_escolha1.get()
             caixa2 = caixa_escolha2.get()
 
-            x = df_full.loc[(df_full['Código'] == f'{caixa1}') & (df_full['BRL'] == f'{caixa2}')]
-            print(x)
-            df_full.loc[2]
-            df1 = df_full.loc[2, [f'{caixa1}']]
-            df2 = df_full.loc[2, [f'{caixa2}']]
+            df_full.set_index('Código', inplace=True)
 
-            for l in enumerate(df1):
-                valor_1 = l[1]
+            df1 = df_full.loc[f'{caixa1}', f'{caixa2}']
 
-            for l in enumerate(df2):
-                valor_2 = l[1]
+            valor_1 = df_full.loc[caixa1, caixa2]
+            print(valor_1)
+            valor_2 = df_full.loc[caixa2, caixa1]
+            print(valor_2)
 
             calc = valor_1 * valor_2
 
             Label(self, text=f'{caixa_escolha2.get()}    {calc}', fg='blue').grid(row=4, column=1)
-            Label(self, text=f'Valor {caixa_escolha1.get()} atual:    {valor_1}', fg='red').grid(row=2, column=1)
-            Label(self, text=f'Valor {caixa_escolha2.get()} atual:   {valor_2}', fg='red').grid(row=3, column=1)
+            Label(self, text=f'Valor do {caixa_escolha1.get()} perante {caixa_escolha2}:    {valor_1}', fg='red').grid(row=2, column=1)
+            Label(self, text=f'Valor do {caixa_escolha2.get()} perante {caixa_escolha1}:   {valor_2}', fg='red').grid(row=3, column=1)
 
         bol_val1 = DoubleVar()
         bol_val2 = DoubleVar()
@@ -81,86 +78,3 @@ class Aplicacao(Frame):
         caixa_escolha2.grid(row=1, column=3)
         caixa_escolha2.current(1)
 
-
-from tkinter import *
-from tkinter import ttk
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-from time import sleep
-from bs4 import BeautifulSoup
-import pandas as pd
-
-
-class Aplicacao(Frame):
-    def __init__(self, meumaster):
-        super().__init__()
-        self['height'] = 150
-        self['width'] = 200
-        self['bd'] = 2
-        self['relief'] = SOLID
-
-        # Tendo acesso aos dados web do site
-        url = 'https://br.investing.com/currencies/exchange-rates-table'
-        option = Options()
-        option.headless = True
-        driver = webdriver.Chrome(options=option)
-        driver.get(url)
-
-        # Encontrando o elemento no site
-        elemento = driver.find_element_by_xpath('//div[@class="wrapper"]//section//table[@id="exchange_rates_1"]')
-        html_content = elemento.get_attribute('outerHTML')
-
-        # Analisando e Extraindo dados
-        souap = BeautifulSoup(html_content, 'html.parser')
-        table = souap.find(name='table')
-
-        # Finalizando
-        df_full = pd.read_html(str(table).replace(',', '.'))[0]
-        sleep(5)
-
-        def conta():
-            print(df_full)
-            df_full.head()
-            caixa1 = caixa_escolha1.get()
-            caixa2 = caixa_escolha2.get()
-
-            x = df_full.loc[(df_full['Código'] == f'{caixa1}') & (df_full['BRL'] == f'{caixa2}')]
-            print(x)
-            df_full.loc[2]
-            df1 = df_full.loc[2, [f'{caixa1}']]
-            df2 = df_full.loc[2, [f'{caixa2}']]
-
-            for l in enumerate(df1):
-                valor_1 = l[1]
-
-            for l in enumerate(df2):
-                valor_2 = l[1]
-
-            calc = valor_1 * valor_2
-
-            Label(self, text=f'{caixa_escolha2.get()}    {calc}', fg='blue').grid(row=4, column=1)
-            Label(self, text=f'Valor {caixa_escolha1.get()} atual:    {valor_1}', fg='red').grid(row=2, column=1)
-            Label(self, text=f'Valor {caixa_escolha2.get()} atual:   {valor_2}', fg='red').grid(row=3, column=1)
-
-        bol_val1 = DoubleVar()
-        bol_val2 = DoubleVar()
-
-        Label(self, text='Valor de origem:').grid(row=0, column=0)
-        valor1 = Entry(self, textvariable=bol_val1)
-        valor1.grid(row=0, column=1)
-
-        Label(self, text='Cotação de destino:').grid(row=1, column=0)
-        valor2 = Entry(self, textvariable=bol_val2)
-        valor2.grid(row=1, column=1)
-
-        Button(self, text='Somar', command=conta).grid(row=2, column=0)
-        Label(self, text='').grid(row=4, column=0)
-        Label(self, text='Convertido para:').grid(row=4, column=0)
-
-        caixa_escolha1 = ttk.Combobox(self, values=['BRL', 'USD', 'EUR', 'GBA', 'JPY', 'CHF', 'CAD', 'AUT'], width=4)
-        caixa_escolha1.grid(row=0, column=3)
-        caixa_escolha1.current(1)
-
-        caixa_escolha2 = ttk.Combobox(self, values=['BRL', 'USD', 'EUR', 'GBA', 'JPY', 'CHF', 'CAD', 'AUT'], width=4)
-        caixa_escolha2.grid(row=1, column=3)
-        caixa_escolha2.current(1)
